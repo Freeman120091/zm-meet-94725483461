4e12e3192c178fcc68388de7256247097287330e
Freeman120091 <elias.wigan@outlook.com>
2025-05-28 23:54:16 +0100

    Update bookMeeting.tsx

--- src/components/bookMeeting.tsx
+++ src/components/bookMeeting.tsx
@@ -1,10 +1,10 @@
 import { useRef, useState } from 'react'
 import InputField from './input'
-import { FaRegCopy } from 'react-icons/fa6';
-import { encodeURL, validateEmail } from '../utils/common';
+import { validateEmail } from '../utils/common';
+import { useNavigate } from 'react-router-dom';
 
 const BookMeeting = () => {
-    const [meetingLink, setMeetingLink] = useState("");
+   const navigate = useNavigate();
     const companyNameRef = useRef<HTMLInputElement>(null);
     const emailRef = useRef<HTMLInputElement>(null);
 
@@ -15,9 +15,9 @@ const BookMeeting = () => {
         if (emailRef.current?.value && !validateEmail(emailRef.current?.value))
           return alert('Please enter a valid email.')
 
-        const link = `/meeting?company=${companyNameRef.current?.value}&email=${emailRef.current?.value}`
-
-        setMeetingLink(encodeURL(link))
+        setTimeout(() => {
+            navigate(`/meeting?company=${companyNameRef.current?.value}&email=${emailRef.current?.value}`, {state: {email: emailRef.current?.value, company: companyNameRef.current?.value}})
+        }, 500);
     };
 
     return (
@@ -37,14 +37,14 @@ const BookMeeting = () => {
                     ref={emailRef}
                 />
                 <div onClick={handleSubmit} className='bg-[#0024f2] hover:bg-[#004bb5] hover:drop-shadow-lg hover:-translate-y-1 cursor-pointer rounded-full text-center py-2 font-semibold mt-5'>BOOK MEETING</div>
-                <div className='text-black text-center text-xl font-bold'>Link Generated:</div>
+                {/* <div className='text-black text-center text-xl font-bold'>Link Generated:</div>
                 {meetingLink && <a href={meetingLink} target='_blank' className='text-[#0024f2] text-center text-sm underline'>{meetingLink}</a> }
                 <div className='flex flex-row justify-center w-full'>
                     <div className='border-2 border-[#0024f2] text-[#0024f2] hover:text-white flex flex-row items-center gap-3 hover:bg-[#004bb5] hover:shadow-lg cursor-pointer rounded-full text-center py-2 px-8 font-semibold'>
                         <FaRegCopy color='black'/>
                         <span>COPY MEETING LINK</span>
                     </div>
-                </div>
+                </div> */}
             </div>
         </div>
     )
